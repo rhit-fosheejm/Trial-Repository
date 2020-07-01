c5ce006722bba85ad1326df931cbe07872d54a04
Ujjaval <ujaptel28199@gmail.com>
2020-07-01 19:18:56 -0400

    Replace Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>New React Native Project</Text>
+      <Text>
+        Add One more line that confirms you are able to pull and push the code
+        (Ujjaval)
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
