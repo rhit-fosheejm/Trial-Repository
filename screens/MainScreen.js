2a409e39eafbb7c70265829e44ec84541af9d108
fosheejm <Fosheejm@rose-hulman.edu>
2020-07-14 20:43:10 -0400

    Direction Box

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,5 +1,4 @@
 
-// export default MainScreen;
 // import React, { Component, useState, useEffect } from "react";
 // import MapViewDirections from 'react-native-maps-directions';
 // import { locations } from "./Data";
@@ -18,29 +17,52 @@
 // import MapView, {
 //     PROVIDER_GOOGLE, Marker
 //   } from "react-native-maps";
-// import GooglePlacesAutocomplete from "../component/GooglePlacesAutocomplete";
 // import Constants from 'expo-constants';
 // import * as Location from 'expo-location';
+// // import Search from "../component/Search";
+// import AutoCompleteBox from '../component/AutoCompleteBox';
+// import AutoCompleteResultList from '../component/AutoCompleteResultList';
+// import { getPredictionWithDetail } from '../services.js';
 
 
 // const GOOGLE_MAPS_APIKEY = "AIzaSyCkNvrH4iYOhuIrUsmFE1bEgDCBJSTX9Fg";
 
 // const coordinates = [
 //   {
-//   latitude:  null,
-//   longitude:null,
+//   latitude:  39.85691,
+//   longitude:-86.27996,
 // },
 // {latitude:  39.75691,
 //   longitude: -86.17996
 // }]
 
 
-// // export default class MainScreen extends React.Component{
-
-//   //  render(){
+// export default class MainScreen extends React.Component{
+//   constructor(props) {
+//     super(props);
+//   this.state = {
+//     query: '',
+//     data: [],
+//     loading: false
+//   };
+// }
+// onChangeText(query) {
+//   this.setState({ query });
+//   getPredictionWithDetail(query, KEY).then(result => {
+//     this.setState({ data: result, loading: false });
+//   });
+// }
+//    render(){
 //     return (
 //     <View style={styles.container}>
-//     <DestinationButton />
+//     {/* <DestinationButton /> */}
+//     <AutoCompleteBox
+//           placeholder="Masukan Nama Tempat"
+//           value={this.state.query}
+//           onChangeText={query => this.onChangeText(query)}
+//         />
+//         <AutoCompleteResultList data={this.state.data} />
+//     {/* <Search/> */}
 // <MapView
 // style={{ flex: 1 }}
 //         showsUserLocation
@@ -66,7 +88,7 @@
 // </MapView>
 //     </View>
 //   );
-// };
+// };}
 
 // const styles = StyleSheet.create({
 //   container: {
@@ -78,17 +100,51 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React, { Component } from "react";
 import { AppRegistry, StyleSheet, Dimensions, Image, View, StatusBar, TouchableOpacity } from "react-native";
 import MapViewDirections from "react-native-maps-directions";
 // import Search from "../component/Search";
 import { DestinationButton } from "../component/DestinationButton";
+import Geojson from 'react-native-geojson';
+import AutoCompleteBox from '../component/AutoCompleteBox';
+import AutoCompleteResultList from '../component/AutoCompleteResultList';
+import { getPredictionWithDetail } from '../services.js';
 
 import MapView from 'react-native-maps';
 import Polyline from '@mapbox/polyline';
 
+
+const alcatraz = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Point',
+        coordinates: [-122.42305755615234, 37.82687023785448],
+      }
+    }
+  ]
+};
 const GOOGLE_MAPS_APIKEY = "AIzaSyCkNvrH4iYOhuIrUsmFE1bEgDCBJSTX9Fg";
-export class MainScreen extends React.Component{
+// export class MainScreen extends React.Component{
+export default class MainScreen extends React.Component{
 // class MainScreen extends Component {
   constructor(props) {
     super(props);
@@ -102,12 +158,23 @@ export class MainScreen extends React.Component{
       x: 'false',
       cordLatitude:39.76,
       cordLongitude: -86.14,
+     query: '',
+    data: [],
+    loading: false
     };
 
     this.mergeLot = this.mergeLot.bind(this);
 
   }
 
+  onChangeText(query) {
+  this.setState({ query });
+  getPredictionWithDetail(query, KEY).then(result => {
+    this.setState({ data: result, loading: false });
+  });
+}
+
+  
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
        (position) => {
@@ -165,16 +232,24 @@ export class MainScreen extends React.Component{
 
     return (
 
-      
+      <View style = {styles.map}>
+      <AutoCompleteBox
+          placeholder="Masukan Nama Tempat"
+          value={this.state.query}
+          onChangeText={query => this.onChangeText(query)}
+        />
+        <AutoCompleteResultList data={this.state.data} />
       <MapView 
         apikey={GOOGLE_MAPS_APIKEY}
+        
       style={styles.map} initialRegion={{
        latitude:39.76691,
        longitude: -86.14996,
-       latitudeDelta: 0.1,
-       longitudeDelta: 0.1
+       latitudeDelta: 5,
+       longitudeDelta: 5
       }}>
     
+    <Geojson geojson={alcatraz} />
       {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
          coordinate={{"latitude":this.state.latitude,"longitude":this.state.longitude}}
          title={"Your Location"}
@@ -200,6 +275,7 @@ export class MainScreen extends React.Component{
        
         
       </MapView>
+      </View>
     );
   }
 }
@@ -210,9 +286,19 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 0
   },
+// container: {
+//   flex: 1,
+
+//   backgroundColor: "#fff",
+// },
 });
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
 
-export default MainScreen;
+//     backgroundColor: "#fff",
+//   },
+// });
